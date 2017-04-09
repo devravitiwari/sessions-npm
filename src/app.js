811235e4f59cf35ed5d8faccb4500d72e5f04497
Ravi Tiwari <ravi.tiwari@tothenew.com>
2017-04-09 17:42:30 +0530

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,4 @@
-console.log("\n\n");
-
+console.log("\n");
 console.log("Introduction to npm");
 console.log("===================");
-
 console.log("\n\n");
-
-
-
-// var express = require('express');
-// var app = express();
-
-// var server = app.listen(0, "localhost", function () {
-//   var host = server.address().address;
-//   var port = server.address().port;
-//   console.log("Listening at http://%s:%s", host, port);
-// });
-
-// app.get('/', function (req, res) {
-//   res.send('Welcome to TO The New');
-// });
